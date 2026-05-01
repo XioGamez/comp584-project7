3a52bf9c366082f1d62acbc7484d2deeeb601433
XioGamez <xiomaragamez2003@gmail.com>
2026-05-01 14:54:11 -0700

    done day 3 part 2

--- day_3.js
+++ day_3.js
@@ -7,18 +7,33 @@ const data = fs.readFileSync("input/day_3_input.txt", "utf8")  // read file as s
 const width = data[0].length ; // store width (used for horizonal wrapping)
 const height = data.length;  // store height (number of rows)
 
-// starting positions
-let x = 0;
-let y = 0;
+function countTrees (dx, dy) {  // dx = steps right, dy = steps down
+    let x = 0;  // horizontal
+    let y = 0;  // vertical
+    let count = 0;  // tree counter
 
-let count = 0;  // tree counter (#)
+    while (y < height) {
+        if (data[y][x % width] === "#")  // if current position contains tree
+            count++;  // increment tree counter
 
-while (y < height) {
-    if (data[y][x % width] === "#")  // if current position contains tree
-        count++;  // increment tree counter
-
-    // move according to slope (right 3, down 1)
-    x += 3;
-    y ++;
+        // move according to slope
+        x += dx;
+        y += dy;
+    }
+    return count;
 }
-console.log("Trees encounters", count);  // print results
+
+const slopes = [  // all slopes to test
+    [1, 1],
+    [3, 1],  // original slope
+    [5, 1],
+    [7, 1],
+    [1, 2]
+];
+
+let result = 1;
+
+for (let [dx, dy] of slopes) {  // loop through each slope
+    result *= countTrees(dx, dy);  // multiply tree count for each slope
+}
+console.log("Trees encountered:", result);  // print results
