b6f21cd52caa38a4649736dff13324166f580100
XioGamez <xiomaragamez2003@gmail.com>
2026-05-01 14:18:17 -0700

    done day 2 part 2

--- day_2.js
+++ day_2.js
@@ -10,12 +10,18 @@ for (let line of lines) {  // loop through each line
 
     const [rule, pwd] = line.split(': ');  // split into rule & pwd (e.g. "1-3 a: abcde" → ["1-3 a", "abcde"])
     const [range, letter] = rule.split(' ');  // split rule into range & letter (e.g. "1-3 a" → ["1-3", "a"])
-    const [min, max] = range.split('-').map(Number);  // split range into min & max numbers (e.g "1-3" → ["1", "3"])
+
+    // part 1: const [min, max] = range.split(' ).map(Number);  // split range into min & max numbers (e.g "1-3" → ["1", "3"])
+    const [pos1, pos2] = range.split('-').map(Number);  // extract positions -> convert to numbers (part 2)
 
     // count times letter appears in pwd
     const count = [...pwd].filter(char => char === letter).length;  // pwd to array (using spread syntax; https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax) -> filter matching letters (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter) -> get length
 
-    if (count >= min && count <= max)  // if count is within allowed range
+    // part 1:
+    // if (count >= min && count <= max)  // if count is within allowed range
+    //     valid++;  // increment count
+
+    if ((pwd[pos1-1] === letter && pwd[pos2 - 1] !== letter) || (pwd[pos1 - 1] !== letter && pwd[pos2 - 1] === letter))  // if pos1 matches, pos2 doesn't OR pos1 doesn't, pos2 matches
         valid++;  // increment count
 }
 console.log("Valid passwords: ", valid);  // print results
